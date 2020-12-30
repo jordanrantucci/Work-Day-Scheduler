edb9ce85e68294218297c909800092739912c467
Jordan Rantucci <jordanrantucci@gmail.com>
2020-12-30 00:46:44 -0500

    working on local storage aspect

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,14 @@
 $(document).ready(function() { // this is used to let the HTML load before calling the JS
 
-    const timeDate = $("<div>") // this creates a variable and generates a div
+    const timeDate = $("<div>")
+    timeDate.attr("class", "time-block") // this creates a variable and generates a div
     $(".jumbotron").append(timeDate); // this appends the div to the jumbotron (top of the page)
     
     function timeDisplay () { //a function for time display
         const timeBlock = moment().format('MMMM Do YYYY, h:mm:ss a') //the moment syntax was taken from the moment.js library
         timeDate.text(timeBlock) //this allows the time to appear as text
     } 
+    timeDisplay();
     setInterval(timeDisplay, 1000) // set interval to 1000 for 1,000 ms per second
 
     const timeArray = [{time: "9:00 AM", key:9},{time: "10:00 AM", key:10}, {time: "11:00 AM", key:11},
@@ -21,7 +23,7 @@ $(document).ready(function() { // this is used to let the HTML load before calli
             row.attr("class", "row hour");
             container.append(row);
             const timeColumn = $("<div>");
-            timeColumn.attr("class", "col-3");
+            timeColumn.attr("class", "col-3"); // this sets the column width
             timeColumn.text(timeArray[i].time);
             timeColumn.css("font-size", "30px");
             timeColumn.css("text-align", "center");
@@ -29,15 +31,31 @@ $(document).ready(function() { // this is used to let the HTML load before calli
             // this section creates the event description column
             const descriptionColumn = $("<textarea>");
             descriptionColumn.attr("class", "col-7 input");
+            descriptionColumn.attr("id", "textbox" + timeArray[i].key)
             row.append(descriptionColumn)
+            // this section creates the save button column
             const saveButton = $("<button>");
             saveButton.attr("class", "col-2 saveBtn");
+            saveButton.attr("id", "savebox" + timeArray[i].key)
+            const image = $("<img>")
+            image.attr("src", "./assets/save.jpg")//this adds a save image
+            image.css("height", "65px")
+            saveButton.append(image)
             row.append(saveButton);
-
         }
-
     }
     containerDisplay();
+
+    function saveButton() {
+        for(var i = 0; i < timeArray.length; i++){
+            const saveButton = $("#savebox" + timeArray[i].key);
+            saveButton.on("click", function(){
+                const textBoxStorage = $("#textbox" + this[i].key)
+                localStorage.setItem("input" + timeArray[i].key, textBoxStorage.value);
+            })
+        }
+    }
+    saveButton();
 
 
 
